3f6715060b2b741d216875f7c3614f0e4d84d526
Jay Santos <jaysant15@gmail.com>
2022-04-27 15:52:48 -0300

    Foi criado um link que direciona para uma pagina vazia

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
+import ShoppingCard from './pages/ShoppingCard';
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={ Home } />
+            <Route extc path="/" component={ ShoppingCard } />
           </Switch>
         </BrowserRouter>
       </div>
